e7dc06aa5fd1d6e9cca9d96ca0d91d8508466a47
Riv2oo4 <riv22500@uvg.edu.gt>
2024-09-29 23:15:15 -0600

    Update material.rs

--- src/material.rs
+++ src/material.rs
@@ -5,8 +5,8 @@ pub struct Material {
     pub specular: f32,
     pub albedo: [f32; 4],
     pub refractive_index: f32,
-    pub emission: Color, // Color de la emisión
-    pub is_emissive: bool, // Indica si el material es emisivo
+    pub emission: Color, 
+    pub is_emissive: bool, 
 }
 
 impl Material {
@@ -15,8 +15,8 @@ impl Material {
         specular: f32,
         albedo: [f32; 4],
         refractive_index: f32,
-        emission: Color, // Añadimos el color de emisión
-        is_emissive: bool, // Indicador de si es emisivo o no
+        emission: Color,
+        is_emissive: bool, 
     ) -> Self {
         Material {
             diffuse,
@@ -28,14 +28,13 @@ impl Material {
         }
     }
 
-    // Método para crear un material no emisivo
     pub fn black() -> Self {
         Material {
             diffuse: Color::new(0, 0, 0),
             specular: 0.0,
             albedo: [0.0, 0.0, 0.0, 0.0],
             refractive_index: 0.0,
-            emission: Color::black(), // Sin emisión
+            emission: Color::black(), 
             is_emissive: false,
         }
     }
